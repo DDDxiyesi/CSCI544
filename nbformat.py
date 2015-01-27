7cea21e616f5143d217e4ddd6a3f731a7dfdf610
DDDxiyesi <liu826@usc.edu>
2015-01-27 00:30:53 -0800

    formatting

--- nbformat.py
+++ nbformat.py
@@ -1,3 +1,30 @@
 import sys
+
+import  os
+import  os.path
+import fnmatch
+
+#coding=utf-8
+
 if __name__ == "__main__":
-	
+	rootdir = '/home/yang/Desktop/csci544-hw1/'
+	spamtrainingdir = rootdir+'SPAM_training/'
+	spamtrainingfile = open(rootdir+'spam_training.txt','w')
+	for file in os.listdir(spamtrainingdir):
+		if fnmatch.fnmatch(file, 'HAM.*'):
+			filedir = spamtrainingdir+file
+			inputfile = open(filedir,'r', errors = 'ignore')
+			spamtrainingfile.write('HAM ')
+			for line in inputfile:
+				spamtrainingfile.write(line.rstrip()+' ')
+			spamtrainingfile.write('\n')
+			inputfile.close()
+		elif fnmatch.fnmatch(file, 'SPAM.*'):
+			filedir = spamtrainingdir+file
+			inputfile = open(filedir,'r', errors = 'ignore')
+			spamtrainingfile.write('SPAM ')
+			for line in inputfile:
+				spamtrainingfile.write(line.rstrip()+' ')
+			spamtrainingfile.write('\n')
+			inputfile.close()
+	spamtrainingfile.close()

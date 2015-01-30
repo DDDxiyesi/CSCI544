8743da544cd9626faaae8473520bdaf4d62c8593
DDDxiyesi <liu826@usc.edu>
2015-01-29 23:19:25 -0800

    P in log

--- nbformat.py
+++ nbformat.py
@@ -1,10 +1,8 @@
 import sys
-
 import  os
 import  os.path
 import fnmatch
 
-#coding=utf-8
 
 if __name__ == "__main__":
 	rootdir = '/home/yang/Desktop/csci544-hw1/'
@@ -16,7 +14,8 @@ if __name__ == "__main__":
 			inputfile = open(filedir,'r', errors = 'ignore')
 			spamtrainingfile.write('HAM ')
 			for line in inputfile:
-				spamtrainingfile.write(line.rstrip()+' ')
+				if line != '\n':
+					spamtrainingfile.write(line.rstrip()+' ')
 			spamtrainingfile.write('\n')
 			inputfile.close()
 		elif fnmatch.fnmatch(file, 'SPAM.*'):
@@ -24,7 +23,11 @@ if __name__ == "__main__":
 			inputfile = open(filedir,'r', errors = 'ignore')
 			spamtrainingfile.write('SPAM ')
 			for line in inputfile:
-				spamtrainingfile.write(line.rstrip()+' ')
+				if line != '\n':
+					spamtrainingfile.write(line.rstrip()+' ')
 			spamtrainingfile.write('\n')
 			inputfile.close()
 	spamtrainingfile.close()
+	# spamtrainingfile = spamtrainingfile = open(rootdir+'spam_training.txt','r')
+	# s = spamtrainingfile.read()
+	# print(s)
--- nblearn.py
+++ nblearn.py
@@ -1,4 +1,5 @@
 import sys
+import math
 if __name__ == "__main__":
 	# Initial
 	spamtrainingfilePath = '/home/yang/Desktop/csci544-hw1/spam_training.txt'
@@ -6,16 +7,19 @@ if __name__ == "__main__":
 	spamtrainingfile = open(spamtrainingfilePath,'r')
 	modelfile = open(modelfilePath,'w')
 	classdic = {}
+	labelcount = {}
 	featuredic = {}
 	for line in spamtrainingfile:
 		if line != '\n':	
 			string = line
 			label = string.partition(' ')[0]	#find label
 			if label not in classdic and label != '\n':
+				labelcount[label] = 0
 				classdic[label] = 1
 			elif label != '\n':
 				classdic[label] +=1
 			thisline = string.split()			#get the line
+			labelcount[label] += len(thisline)-1
 			for i in range(1,len(thisline)):	#from 2nd letter to end of line
 				word = thisline[i]				#the No.i word
 				if word not in featuredic:		#the word not in dic
@@ -26,8 +30,29 @@ if __name__ == "__main__":
 						featuredic[word][label] = 1
 					else:
 						featuredic[word][label] += 1
+	#count = 0
+	spamtrainingfile.close()
+
+	labelsum = 0
+	for label in classdic:
+		labelsum += classdic[label]
+	print(labelsum)
+	for label in classdic:
+		classdic[label] = math.log(classdic[label])-math.log(labelsum)
+	for feature in featuredic:
+		for label in featuredic[feature]:
+			if label in labelcount:
+				featuredic[feature][label] = math.log(featuredic[feature][label])-math.log(labelcount[label])
 	modelfile.write(str(classdic)+'\n')
+	modelfile.write(str(labelcount)+'\n')
+	
+
 	for feature in featuredic:
 		modelfile.write(str(feature)+' ')
 		modelfile.write(str(featuredic[feature]))
 		modelfile.write('\n')
+	# 	for label in featuredic[feature]:
+	# 		count+=featuredic[feature][label]
+	# print(count)
+	
+
